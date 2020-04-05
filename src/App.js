547676709d48ddbaef4dc557535814052ac85fb0
ayoakpata <akpata.ayobami@gmail.com>
2020-04-05 10:29:41 -0700

    getting user profiles

--- src/App.js
+++ src/App.js
@@ -19,25 +19,84 @@ class App extends Component {
     clientID: 'adPowHSgvC2MiQiZnmgv0mLXG1gnx66q',
     domain: 'dev-4k-3rwkf.auth0.com'
   };
-
+  //user not signed in
   componentWillMount() {
     this.lock = new Auth0Lock(this.props.clientID, this.props.domain);
     this.lock.on('authenticated', authResult => {
-      console.log(authResult);
+      //console.log(authResult);
+
+      this.lock.getProfile(authResult.idToken, (error, profile) => {
+        if (error) {
+          console.log(error);
+          return;
+        }
+        //console.log(profile);
+        this.setProfile(authResult.idToken, profile);
+      });
     });
+    //user signed in. get profile
+    this.getProfile();
+  }
+
+  setProfile(idToken, profile) {
+    localStorage.setItem('idToken', idToken);
+    localStorage.setItem('profile', JSON.stringify(profile));
+
+    this.setState({
+      idToken: localStorage.getItem('idToken'),
+      profile: JSON.parse(localStorage.getItem('profile'))
+    });
+  }
+
+  getProfile() {
+    if (localStorage.getItem('idToken') != null) {
+      this.setState(
+        {
+          idToken: localStorage.getItem('idToken'),
+          profile: JSON.parse(localStorage.getItem('profile'))
+        },
+        () => {
+          console.log(this.state);
+        }
+      );
+    }
   }
 
   showLock() {
     this.lock.show();
   }
 
+  logout() {
+    this.setState(
+      {
+        idToken: '',
+        profile: ''
+      },
+      () => {
+        localStorage.removeItem('idToken');
+        localStorage.removeItem('profile');
+      }
+    );
+  }
+
   render() {
+    let loggedIn;
+
+    if (this.state.idToken) {
+      loggedIn = <Github />;
+    } else {
+      loggedIn = 'please log in';
+    }
     return (
       <div className='App'>
-        <Thetopper onLogin={this.showLock.bind(this)}/>
-        <div>
-          <Github />
-        </div>
+        <Thetopper
+          lock={this.lock}
+          idToken={this.state.idToken}
+          profile={this.state.profile}
+          onLogout={this.logout.bind(this)}
+          onLogin={this.showLock.bind(this)}
+        />
+        {loggedIn}
       </div>
     );
   }
--- src/Components/Thetopper.js
+++ src/Components/Thetopper.js
@@ -2,13 +2,33 @@ import React, { Component } from 'react';
 import { Navbar, Nav, NavItem } from 'react-bootstrap';
 
 class Thetopper extends Component {
+  onLogin() {
+    this.props.onLogin();
+  }
+
+  onLogout() {
+    this.props.onLogout();
+  }
+
   render() {
+    let logpage;
+    if (this.props.idToken) {
+      logpage = (
+        <NavItem onClick={this.onLogin.bind(this)} href='#'>
+          logout
+        </NavItem>
+      );
+    } else {
+      logpage = (
+        <NavItem onClick={this.onLogin.bind(this)} href='#'>
+          login
+        </NavItem>
+      );
+    }
     return (
       <Navbar>
         <Navbar.Brand>searcher-logo</Navbar.Brand>
-        <Nav>
-          <a href='#'>login</a>
-        </Nav>
+        <Nav>{logpage}</Nav>
       </Navbar>
     );
   }
